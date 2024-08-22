d527d504f02e370825de8f3f89fd4d6befba2ff6
DrRoot85 <mhbmis@icloud.com>
2024-08-22 12:15:57 +0300

    My Last Build

--- config.h
+++ config.h
@@ -69,24 +69,24 @@ static const char unknown_str[] = "n/a";
 // };
 static const struct arg args[] = {
 	/* function format          argument */
-	{ uptime,       " %s |",           NULL },
+	// { uptime,       " %s |",           NULL },
 
 	{ battery_state,    " %s |",           "BAT0" },
 	{ battery_perc,     " %s%% |",         "BAT0" },
 	{ battery_remaining,     " (%s)|",         "BAT0" },
 
-	{ ram_used,     " %s |",           NULL },
-	{ ram_perc,     " (%s%%)|",            NULL },
+	// { ram_used,     " %s |",           NULL },
+	// { ram_perc,     " (%s%%)|",            NULL },
 
-	{ cpu_perc,     " %s%%|",          NULL },
+	// { cpu_perc,     " %s%%|",          NULL },
 
-	{ disk_used,        " %.4s |",         "/" },
+	// { disk_used,        " %.4s |",         "/" },
 /*  { disk_total,   "DISKT%.4s GiB |",      "/" },*/
 
 	{ wifi_essid,       " %s |",           "wlan0" },
 //  { wifi_perc,        " (%s%%)|",            "wlan0" },
 
-	{ keymap,       " %s |",       NULL },
+	{ keymap,       "  %s |",       NULL },
 
 	{ datetime,     " %s ",        "%F %r " },
 
